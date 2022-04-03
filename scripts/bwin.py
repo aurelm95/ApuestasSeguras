cd6d16b0cbd10d5407ad71a24f56aa417c3c1596
Aure <aurelm95@gmail.com>
2022-04-03 15:15:35 +0200

    mejor try except en bwin

--- scripts/bwin.py
+++ scripts/bwin.py
@@ -42,51 +42,50 @@ class Bwin(CasaDeApuestas):
 		self.DATA=[]
 		self.respuesta = self.s.post('https://cds-api.bwin.es/bettingoffer/lobby/sport', headers=self.headers, params=self.params, data=self.data)
 		self.j=self.respuesta.json()
+		logger.debug("Status code: "+str(self.respuesta.status_code))
 
-		for p in self.j['highlights']:
-			# logger.debug(f"{p['games']=}")
-			e1=p['games'][0]['results'][0]
-			# Note that due to the usual issues with binary floating-point (see Floating Point Arithmetic: Issues and Limitations), the argument to Fraction(1.1) is not exactly equal to 11/10, and so Fraction(1.1) does not return Fraction(11, 10) as one might expect.
-			# https://docs.python.org/2/library/fractions.html
-			# He hecho una chapucilla
-			odds1=Fraction(int(e1['odds']*100))/100
-			j1=e1['name']['value']
-			e2=p['games'][0]['results'][1]
-			odds2=Fraction(int(e2['odds']*100))/100
-			j2=e2['name']['value']
-			unix_timestamp=None
-			try:
-				unix_timestamp=int(datetime.strptime(p['startDate'], "%Y-%m-%dT%H:%M:%SZ").timestamp())
-			except Exception as e:
-				logger.timestamp_warning("No se ha podido parsear la fecha: "+str(e)+" string original: "+p['startDate'])
-			
-			def parsear_juagador_dobles(s):
+		def parsear_juagador_dobles(s):
+			nombre,apellido=s.rsplit('. ',1)
+			if len(nombre)==1:
+				return Jugador(inicial_nombre=nombre,apellido=apellido,web=self.nombre)
+			return Jugador(nombre=nombre,apellido=apellido,web=self.nombre)
+		
+		def parsear_juagador(s):
+			if '(' in s:
+				s=s.split('(')[0] # A veces los nombres contienen entre parentesis la nacionalidad. por ejmeplo: Clement Tabur (FRA) o bien: Yuta Shimizu (JPN)
+			if s[-1]==' ':
+				s=s[:-1] # posiblemente tras el split de arriba quede un espacio final. Lo quito.
+			if '. ' in s:
 				nombre,apellido=s.rsplit('. ',1)
+				if apellido=='': logger.error(s+" -> apellido=''")
 				if len(nombre)==1:
 					return Jugador(inicial_nombre=nombre,apellido=apellido,web=self.nombre)
 				return Jugador(nombre=nombre,apellido=apellido,web=self.nombre)
-			
-			def parsear_juagador(s):
-				if '(' in s:
-					s=s.split('(')[0] # A veces los nombres contienen entre parentesis la nacionalidad. por ejmeplo: Clement Tabur (FRA) o bien: Yuta Shimizu (JPN)
-				if s[-1]==' ':
-					s=s[:-1] # posiblemente tras el split de arriba quede un espacio final. Lo quito.
-				if '. ' in s:
-					nombre,apellido=s.rsplit('. ',1)
-					if apellido=='': logger.error(s+" -> apellido=''")
-					if len(nombre)==1:
-						return Jugador(inicial_nombre=nombre,apellido=apellido,web=self.nombre)
-					return Jugador(nombre=nombre,apellido=apellido,web=self.nombre)
-				elif ' ' in s:
-					nombre,apellido=s.rsplit(' ',1)
-					if apellido=='': logger.error(s+" -> apellido=''")
-					return Jugador(nombre=nombre,apellido=apellido,web=self.nombre)
-				return Jugador(apellido=s,web=self.nombre)
+			elif ' ' in s:
+				nombre,apellido=s.rsplit(' ',1)
+				if apellido=='': logger.error(s+" -> apellido=''")
+				return Jugador(nombre=nombre,apellido=apellido,web=self.nombre)
+			return Jugador(apellido=s,web=self.nombre)
 
-
-
-			dobles=True if '/' in j1 else False
+		for p in self.j['highlights']:
 			try:
+				# logger.debug(f"{p['games']=}")
+				e1=p['games'][0]['results'][0]
+				# Note that due to the usual issues with binary floating-point (see Floating Point Arithmetic: Issues and Limitations), the argument to Fraction(1.1) is not exactly equal to 11/10, and so Fraction(1.1) does not return Fraction(11, 10) as one might expect.
+				# https://docs.python.org/2/library/fractions.html
+				# He hecho una chapucilla
+				odds1=Fraction(int(e1['odds']*100))/100
+				j1=e1['name']['value']
+				e2=p['games'][0]['results'][1]
+				odds2=Fraction(int(e2['odds']*100))/100
+				j2=e2['name']['value']
+				unix_timestamp=None
+				try:
+					unix_timestamp=int(datetime.strptime(p['startDate'], "%Y-%m-%dT%H:%M:%SZ").timestamp())
+				except Exception as e:
+					logger.timestamp_warning("No se ha podido parsear la fecha: "+str(e)+" string original: "+p['startDate'])
+
+				dobles=True if '/' in j1 else False
 				if dobles:
 					e1j1,e1j2=j1.split('/')
 					e2j1,e2j2=j2.split('/')
